fbd4d1c97f7106a1b25494aedddb5664986e6990
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-11 15:27:44 +0200

    The navigation bar has been injected

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import NavigationBar from './components/navigationBar/navigationBar';
 import colors from './styles/colors.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <NavigationBar />
       <StatusBar style="auto" />
     </View>
   );
