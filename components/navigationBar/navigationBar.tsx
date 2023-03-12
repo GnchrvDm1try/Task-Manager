1b7b35111f398bc63133030c8e8190cda0a2de08
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-12 15:38:13 +0200

    Created type for properties of the navigation bar component

--- components/navigationBar/navigationBar.tsx
+++ components/navigationBar/navigationBar.tsx
@@ -3,7 +3,12 @@ import { styles } from './navigationBar.styles';
 import TasksIcon from '../../assets/icons/tasks_icon.svg';
 import CalendarIcon from '../../assets/icons/calendar_icon.svg';
 
-export default function NavigationBar() {
+type NavigationProps = {
+    isHeaderDisplayed?: boolean;
+    children: React.ReactNode;
+}
+
+export default function NavigationBar(props: NavigationProps) {
     return (
         <View style={styles.container}>
             <View style={styles.bar}>
