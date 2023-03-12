f4765a9500e397f0dbcfc7549e180efce00ce667
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-12 17:02:48 +0200

    Created an anonymous function that returns null or the header bar if needed

--- components/navigationBar/navigationBar.tsx
+++ components/navigationBar/navigationBar.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { ReactElement } from "react";
+import { Text, View } from "react-native";
 import { styles } from './navigationBar.styles';
 import TasksIcon from '../../assets/icons/tasks_icon.svg';
 import CalendarIcon from '../../assets/icons/calendar_icon.svg';
@@ -9,6 +10,18 @@ type NavigationProps = {
 }
 
 export default function NavigationBar(props: NavigationProps) {
+    const headerBar = (): ReactElement | null => {
+        if (props.isHeaderDisplayed)
+            return (
+                <View>
+                    <View style={styles.header}>
+                        <Text style={styles.headerText}></Text>
+                    </View>
+                </View>)
+        else
+            return null
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.bar}>
