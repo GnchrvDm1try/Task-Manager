6e79cb4aa54a463eccda7ad75c32601287dce166
Fr1senn <bombavlad1@gmail.com>
2023-03-13 10:16:09 +0200

    The root component design has been changed

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import NavigationBar from './components/navigationBar/navigationBar';
+import TaskItem from "./components/taskItem/taskItem";
 import colors from './styles/colors.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationBar />
-      <StatusBar style="auto" />
+      <NavigationBar>
+        <TaskItem/>
+      </NavigationBar>
     </View>
   );
 }
