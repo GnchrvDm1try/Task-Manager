eae159edcc686ea00f5fcbe77b8d68890ccc3805
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-21 10:16:19 +0200

    Created and implemented a function responsible for the tasks stack navigation

--- navigators/tasksScreenNavigator.tsx
+++ navigators/tasksScreenNavigator.tsx
@@ -1,4 +1,27 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TaskItem from '../components/taskItem/taskItem';
+import colors from '../styles/colors.json';
+
 export type TaskListStackParamList = {
     'Task list': undefined;
     'Task info': { taskId: number };
 };
+
+const Stack = createNativeStackNavigator<TaskListStackParamList>();
+
+export default function TasksScreen() {
+    return (
+        <Stack.Navigator>
+            <Stack.Group
+                screenOptions={{
+                    headerTitleStyle: { fontSize: 30 },
+                    headerStyle: { backgroundColor: colors.primaryColor }
+                }}>
+                <Stack.Screen
+                    name='Task list'
+                    component={TaskItem}
+                    options={{ headerShown: false }} />
+            </Stack.Group>
+        </Stack.Navigator>
+    );
+}
