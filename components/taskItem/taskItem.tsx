b5fde173c9441fbc7c26dc5278c9eba1da03eed1
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-21 10:01:02 +0200

    Added on press handling that redirects to the task info page

--- components/taskItem/taskItem.tsx
+++ components/taskItem/taskItem.tsx
@@ -1,11 +1,17 @@
 import { Text, View, SafeAreaView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { styles } from './taskItem.styles';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { TaskListStackParamList } from '../../navigators/tasksScreenNavigator';
 
-export default function TaskItem() {
+type Props = NativeStackScreenProps<TaskListStackParamList, 'Task list'>;
+
+export default function TaskItem({ navigation }: Props) {
   return (
     <SafeAreaView>
-      <TouchableOpacity style={styles.container}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Task info', { taskId: 1 })}
+        style={styles.container}>
         <View style={{ alignSelf: 'flex-start' }}>
           <Text style={styles.taskDate}>00.00.0000</Text>
         </View>
