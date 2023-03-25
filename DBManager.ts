efd4a0f3dbd37337f61d591563ad4524a85ef208
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-25 16:27:42 +0200

    Implemented methods that create tables 'tasks' and 'stages' if they aren't exist

--- DBManager.ts
+++ DBManager.ts
@@ -1,6 +1,11 @@
+import * as SQLite from 'expo-sqlite';
 export class DBManager {
     private static instance: DBManager;
+    private static readonly db = SQLite.openDatabase('TaskManagerDB')
+
     private constructor() {
+        DBManager.createTasksTableIfNotExists();
+        DBManager.createStagesTableIfNotExists();
     }
 
     public static getInstance(): DBManager {
@@ -9,4 +14,29 @@ export class DBManager {
         return DBManager.instance;
     }
 
+    private static createTasksTableIfNotExists() {
+        DBManager.db.transaction(tx => {
+            tx.executeSql(`CREATE TABLE IF NOT EXISTS Tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                is_done INTEGER CHECK(is_done IN(0, 1)) NOT NULL DEFAULT 0,
+                addition_date TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+                begin_date TIMESTAMP CHECK(begin_date > addition_date),
+                deadline_date TIMESTAMP CHECK(deadline_date > begin_date ));`,
+            );
+        });
+    }
+
+    private static createStagesTableIfNotExists() {
+        DBManager.db.transaction(tx => {
+            tx.executeSql(`CREATE TABLE IF NOT EXISTS Stages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                task_id INTEGER REFERENCES Tasks(id) NOT NULL,
+                title TEXT NOT NULL,
+                is_done INTEGER CHECK(is_done IN (0, 1)) NOT NULL DEFAULT 0,
+                description TEXT,
+                deadline_date TIMESTAMP);`
+            );
+        });
+    }
 }
