8bf578e843eab01f44d01a2f2f599abcc6c3b6d8
Goncharov Dmitry <prototype1357@ukr.net>
2023-03-26 15:26:19 +0300

    Added route that refers to task detail component

--- navigators/tasksScreenNavigator.tsx
+++ navigators/tasksScreenNavigator.tsx
@@ -1,10 +1,12 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TaskDetails from '../components/taskDetails/taskDetails';
 import TaskList from '../components/taskList/taskList';
+import Task from '../models/Task';
 import colors from '../styles/colors.json';
 
 export type TaskListStackParamList = {
     'Task list': undefined;
-    'Task info': { taskId: number };
+    'Task info': { task: Task };
 };
 
 const Stack = createNativeStackNavigator<TaskListStackParamList>();
@@ -21,6 +23,10 @@ export default function TasksScreen() {
                     name='Task list'
                     component={TaskList}
                     options={{ headerShown: false }} />
+                <Stack.Screen
+                    name='Task info'
+                    component={TaskDetails}
+                    options={({ route }) => ({ title: route.params.task.title })} />
             </Stack.Group>
         </Stack.Navigator>
     );
